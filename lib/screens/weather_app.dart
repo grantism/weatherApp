913dae17521b4d7682faee8ede3b94d1bdc5c743
Grant McNally <grantm@worldmanager.com>
2021-01-08 14:12:04 +1100

    Replace starter app screen with static weather details display.

---
+++ lib/screens/weather_app.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class WeatherApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Container(
+          child: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Text('Summer Hill', style: TextStyle(fontSize: 32)),
+          SizedBox(height: 10),
+          Text('Mostly Cloudy', style: TextStyle(fontSize: 20)),
+          SizedBox(height: 40),
+          Text('21\u2103', style: TextStyle(fontSize: 48)),
+        ]),
+      )),
+    );
+  }
+}
