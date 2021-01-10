598d3eabf1f18a74b4435786af0049adc421f8ff
Grant McNally <grantm@worldmanager.com>
2021-01-10 13:42:32 +1100

    Display the API data in the app.

--- lib/screens/weather_app.dart
+++ lib/screens/weather_app.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/network/api_request.dart';
+import 'package:weather_app/models/api_result.dart';
+import 'package:weather_app/models/weather.dart';
 
 class WeatherApp extends StatelessWidget {
   ApiRequest _apiResponse = ApiRequest();
@@ -14,17 +16,33 @@ class WeatherApp extends StatelessWidget {
       ),
       body: Container(
           child: Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Text('Summer Hill', style: TextStyle(fontSize: 32)),
-          SizedBox(height: 10),
-          Text('Mostly Cloudy', style: TextStyle(fontSize: 20)),
-          SizedBox(height: 40),
-          Text('21\u2103', style: TextStyle(fontSize: 48)),
-            FutureBuilder(
-                future: _apiResponse.getWeather()
-            )
-        ]),
-      )),
+              child: FutureBuilder(
+                  future: _apiResponse.getWeather(),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<ApiResult> snapshot) {
+                    if (snapshot.data is SuccessState) {
+                      Weather weather =
+                          (snapshot.data as SuccessState).value;
+                      return weatherBuilder(weather, context);
+                    } else if (snapshot.data is ErrorState) {
+                      String errorMessage = (snapshot.data as ErrorState).msg;
+                      return Text(errorMessage);
+                    } else {
+                      return CircularProgressIndicator();
+                    }
+                  })
+          )),
     );
   }
+}
+
+
+weatherBuilder(weather, context) {
+  return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+    Text(weather.locationName, style: TextStyle(fontSize: 32)),
+    SizedBox(height: 10),
+    Text('Mostly Cloudy', style: TextStyle(fontSize: 20)),
+    SizedBox(height: 40),
+    Text(weather.currentTemp.toString() + '\u2103', style: TextStyle(fontSize: 48)),
+  ]);
 }
